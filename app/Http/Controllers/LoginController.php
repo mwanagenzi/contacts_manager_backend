cf686e0687fd373f2a65d9d32b9f044fb20be688
mwanagenzi <njorogemosesngure@gmail.com>
2023-10-31 09:25:19 +0300

    Setup and integrate auth

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+            'device_name' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+        logger($user);
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            logger($user);
+            return response()->json([
+                'success' => false,
+                'message' => 'Provided credentials are incorrect',
+            ]);
+        }
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Logged in successfully',
+            'token' => $user->createToken($request->device_name)->plainTextToken,
+            'datum' => 'User data',
+        ]);
+
+    }
+}
